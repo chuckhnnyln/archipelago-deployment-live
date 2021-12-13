2e2e648a7a611e4a18d3f6ac6e5874c6d2194df4
Chuck Henry <chuckh@nnyln.org>
2021-12-13 13:49:35 +0000

    Fresh Install of Archipelago for esdam.org

---
+++ drupal_stock/scripts/archipelago/setup_multi.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then echo "No domain specified"; exit 1; fi
+
+echo "Adding Drupal 9 basic Configs"
+chmod 755 /var/www/html/$1/web/sites/default/settings.php
+cat <<EOT >> /var/www/html/$1/web/sites/default/settings.php
+\$MINIO_ACCESS_KEY=getenv("MINIO_ACCESS_KEY");
+\$MINIO_SECRET_KEY=getenv("MINIO_SECRET_KEY");
+\$MINIO_BUCKET_MEDIA=getenv("MINIO_BUCKET_MEDIA");
+\$MINIO_FOLDER_PREFIX_MEDIA=rtrim(getenv("MINIO_FOLDER_PREFIX_MEDIA"), "/");
+\$settings['s3fs.access_key'] = \$MINIO_ACCESS_KEY;
+\$settings['s3fs.secret_key'] = \$MINIO_SECRET_KEY;
+\$config['s3fs.settings']['bucket'] = \$MINIO_BUCKET_MEDIA;
+\$config['s3fs.settings']['root_folder'] = \$MINIO_FOLDER_PREFIX_MEDIA;
+\$settings['s3fs.upload_as_private'] = TRUE;
+\$settings['file_private_path'] = '/var/www/html/$1/private';
+ini_set('memory_limit', '1024M');
+if (PHP_SAPI !== 'cli') {
+  \$settings['reverse_proxy'] = TRUE;
+  \$settings['reverse_proxy_addresses'] = [@\$_SERVER['REMOTE_ADDR']];
+} else {
+  \$settings['reverse_proxy'] = FALSE;
+}
+\$settings['hash_salt'] = '46eb4e1a-5738-41b0-bf2e-f3de4ff8dfb0';
+\$settings['webform_strawberryfield.europeana_entity_apikey'] = 'apidemo';
+if (file_exists(\$app_root . '/' . \$site_path . '/settings.local.php')) {
+   include \$app_root . '/' . \$site_path . '/settings.local.php';
+}
+
+EOT
+echo "Updating your web root folder permissions."
+chmod 0666 /var/www/html/$1/web/sites/default/settings.php
+chown -R www-data:www-data /var/www/html/$1/web/sites
+echo "Downloading JQUERY Slider Pips Library for facets" 
+mkdir -p /var/www/html/$1/web/libraries/jquery-ui-slider-pips/dist
+curl -o /var/www/html/$1/web/libraries/jquery-ui-slider-pips/dist/jquery-ui-slider-pips.min.js 'https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.js' 
+curl -o /var/www/html/$1/web/libraries/jquery-ui-slider-pips/dist/jquery-ui-slider-pips.min.css 'https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.css' 
